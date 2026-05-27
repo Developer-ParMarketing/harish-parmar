ca5c02886355a20a8a63634a05865fa630ad0d58
pranay-parmarketing <pranay.badgujar@parmarketing.agency>
2026-05-27 10:08:15 +0530

    test

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ const corsOptions = {
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
 };
-
+// 
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
